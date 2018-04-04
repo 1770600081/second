4d1ce96b7352ad6f26e7e8b6e02c184217047c6a
付一 <1770600081@qq.com>
2018-04-04 13:03:33 +0800

    备注

---
+++ vendor/yiisoft/extensions.php
@@ -0,0 +1,87 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'yiisoft/yii2-swiftmailer' => 
+  array (
+    'name' => 'yiisoft/yii2-swiftmailer',
+    'version' => '2.0.7.0',
+    'alias' => 
+    array (
+      '@yii/swiftmailer' => $vendorDir . '/yiisoft/yii2-swiftmailer',
+    ),
+  ),
+  'yiisoft/yii2-debug' => 
+  array (
+    'name' => 'yiisoft/yii2-debug',
+    'version' => '2.0.13.0',
+    'alias' => 
+    array (
+      '@yii/debug' => $vendorDir . '/yiisoft/yii2-debug',
+    ),
+  ),
+  'yiisoft/yii2-gii' => 
+  array (
+    'name' => 'yiisoft/yii2-gii',
+    'version' => '2.0.6.0',
+    'alias' => 
+    array (
+      '@yii/gii' => $vendorDir . '/yiisoft/yii2-gii',
+    ),
+  ),
+  'yiisoft/yii2-bootstrap' => 
+  array (
+    'name' => 'yiisoft/yii2-bootstrap',
+    'version' => '2.0.8.0',
+    'alias' => 
+    array (
+      '@yii/bootstrap' => $vendorDir . '/yiisoft/yii2-bootstrap/src',
+    ),
+  ),
+  'yiisoft/yii2-faker' => 
+  array (
+    'name' => 'yiisoft/yii2-faker',
+    'version' => '2.0.4.0',
+    'alias' => 
+    array (
+      '@yii/faker' => $vendorDir . '/yiisoft/yii2-faker',
+    ),
+  ),
+  'crazyfd/yii2-qiniu' => 
+  array (
+    'name' => 'crazyfd/yii2-qiniu',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@crazyfd/qiniu' => $vendorDir . '/crazyfd/yii2-qiniu',
+    ),
+  ),
+  'dzer/yii2-express' => 
+  array (
+    'name' => 'dzer/yii2-express',
+    'version' => '1.3.0.0',
+    'alias' => 
+    array (
+      '@dzer/express' => $vendorDir . '/dzer/yii2-express/src',
+    ),
+  ),
+  'johnnylei/jstree' => 
+  array (
+    'name' => 'johnnylei/jstree',
+    'version' => '1.1.0.0',
+    'alias' => 
+    array (
+      '@johnnylei/jstree' => $vendorDir . '/johnnylei/jstree',
+    ),
+  ),
+  'yii-dream-team/yii2-jstree' => 
+  array (
+    'name' => 'yii-dream-team/yii2-jstree',
+    'version' => '1.0.0.0',
+    'alias' => 
+    array (
+      '@yiidreamteam/jstree' => $vendorDir . '/yii-dream-team/yii2-jstree/src',
+    ),
+  ),
+);
